65dfcb490a5c0599966e0d0260f4bba4a636e3f1
bonneauyo <bonneauyo@gmail.com>
2021-11-19 15:08:41 +0100

    pour abdel

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-import re
-import json
 import urllib
 import glob
+import urllib.request
+import json
 
 from os import read
 from bs4 import BeautifulSoup
 
+INITIAL_ID = 30005
+LAST_ID = 30015
+
 def dowload_all_page():
-    for i in range(0,10):
+    for i in range(INITIAL_ID,LAST_ID):
         link = 'http://echecs.asso.fr/FicheTournoi.aspx?Ref='+str(i)+''
         page = urllib.request.urlopen(link)
         soup = BeautifulSoup(page.read(), 'html.parser')
@@ -16,15 +19,35 @@ def dowload_all_page():
         file.close()
 
 def scraping_all_page():
-    dir = {}
-    i = 0
     for chemin in glob.glob("sample/*"):
         file = open(chemin, encoding="utf8", errors='ignore')
         soup = BeautifulSoup(file.read(), 'html.parser')
         file.close
-        text = soup.find_all("td")[i].text
+        allTd = soup.find_all("td")
+        
+        text = []
+        for word in allTd:
+            text.append(word.text)
+
         print(text)
-        i = i + 1
+        dict1 = {}
+        text = list(filter(None,text))
+        for word in text:
+            if len(word)==0:
+                print(word)
+            else :
+                if word is not text[-1]:
+                    if ':' in word:
+                        print(word)
+                        inext = text.index(word)+1
+                        word = word.strip(':')
+                        dict1[word]= text[inext]
+
+        print(dict1)
+
+        out_file = open("test1.json", "w", encoding="utf-8") 
+        json.dump(dict1, out_file, indent = 4, sort_keys = False, ensure_ascii=False) 
+        out_file.close()
 
 
 
