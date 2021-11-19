f61aad1bb2bcf81fdc14ee8ef7f6efb8f660665e
bonneauyo <bonneauyo@gmail.com>
2021-11-19 15:36:19 +0100

    scraping

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import urllib
 import glob
 import urllib.request
 import json
+import re
+import os.path
+
 
 from os import read
 from bs4 import BeautifulSoup
@@ -20,17 +23,15 @@ def dowload_all_page():
 
 def scraping_all_page():
     for chemin in glob.glob("sample/*"):
+        basename = os.path.splitext(os.path.basename(chemin))
         file = open(chemin, encoding="utf8", errors='ignore')
         soup = BeautifulSoup(file.read(), 'html.parser')
         file.close
         allTd = soup.find_all("td")
-        
         text = []
         for word in allTd:
             text.append(word.text)
-
-        print(text)
-        dict1 = {}
+        dict = {}
         text = list(filter(None,text))
         for word in text:
             if len(word)==0:
@@ -38,48 +39,13 @@ def scraping_all_page():
             else :
                 if word is not text[-1]:
                     if ':' in word:
-                        print(word)
                         inext = text.index(word)+1
                         word = word.strip(':')
-                        dict1[word]= text[inext]
+                        dict[word]= text[inext]
 
-        print(dict1)
-
-        out_file = open("test1.json", "w", encoding="utf-8") 
-        json.dump(dict1, out_file, indent = 4, sort_keys = False, ensure_ascii=False) 
+        out_file = open("json/"+basename[0]+".json", "w", encoding="utf-8") 
+        json.dump(dict, out_file, indent = 4, sort_keys = False, ensure_ascii=False) 
         out_file.close()
-
-
 
 dowload_all_page()
 scraping_all_page()
-
-"""file = open("Fédération Française des Échecs.html")
-soup = BeautifulSoup(file.read(), 'html.parser')
-
-#print(str(soup))
-
-print(soup.find_all('td'))
-
-text = get_paragraphs_BP3(str(soup))
-
-dict1 = {}
-
-text = list(filter(None,text))
-
-for word in text:
-    if len(word)==0:
-        print(word)
-    else :
-        if word is not text[-1]:
-            if ':' in word:
-                inext = text.index(word)+1
-                word = word.strip(':')
-                dict1[word]= text[inext]
-print(dict1)
-
-    
-
-out_file = open("test1.json", "w") 
-json.dump(dict1, out_file, indent = 4, sort_keys = False, ensure_ascii=False) 
-out_file.close()"""
